ecc4d714c66f4270be20dc067a0b1a9497e43274
Sasan Hezarkhani <sasan.hezarkhani@vungle.com>
2016-04-12 10:12:26 -0700

    test

--- src/rebar_cmd_prv.erl
+++ src/rebar_cmd_prv.erl
@@ -30,9 +30,8 @@ init(State) ->
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
   Config = rebar_state:get(State, cmd, []),
-  Args = rebar_state:command_args(State),
-  io:format("~p~n", Config),
-  io:format("~p~n", lists:nth(1, [ Args ])),
+  [Command | Args] = [ rebar_state:command_args(State) ],
+  lists:keyfind("test", 1, Config),
   {ok, State}.
 
 -spec format_error(any()) ->  iolist().
