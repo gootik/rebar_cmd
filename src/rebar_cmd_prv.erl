d527e2f25e4bdf6f1d82624b678bb30f0d63e5a9
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-09-28 00:13:41 +0100

    Use os:cmd instead of Erlang ports

--- src/rebar_cmd_prv.erl
+++ src/rebar_cmd_prv.erl
@@ -13,6 +13,20 @@
 
 -define(PROVIDER, cmd).
 -define(DEPS, [app_discovery]).
+-define(DEFAULT_OPT_TIMEOUT, 15000).
+-define(DEFAULT_OPT_VERBOSE, false).
+
+-ifdef(TEST).
+-export([
+    do_internal/2,
+    find_command_in/2
+]).
+
+-ignore_xref([
+    do_internal/2,
+    find_command_in/2
+]).
+-endif.
 
 %% ===================================================================
 %% Public API
@@ -35,17 +49,8 @@ init(State) ->
 do(State) ->
     Config = rebar_state:get(State, commands, []),
     {ArgsPropList, _} = rebar_state:command_parsed_args(State),
-    {task, CmdName} = proplists:lookup(task, ArgsPropList),
-    case lists:keyfind(list_to_atom(CmdName), 1, Config) of
-        {_, Command} ->
-            rebar_api:debug("Running ~p with command ~p.~n", [[CmdName], [Command]]),
-            case run_shell({CmdName, Command}) of
-                {ok, _} -> {ok, State};
-                {error, Error} -> {error, Error}
-            end;
-        false ->
-            {error, "Command " ++ CmdName ++ " is unknown. Check your configuration."}
-    end.
+    CmdName = proplists:get_value(task, ArgsPropList, undefined),
+    do_internal({CmdName, find_command_in(CmdName, Config)}, State).
 
 -spec format_error(any()) -> iolist().
 format_error(Reason) ->
@@ -54,35 +59,78 @@ format_error(Reason) ->
 %% ===================================================================
 %% Private API
 %% ===================================================================
-run_shell({CmdName, Command}) ->
-    run_shell({CmdName, Command}, 15000).
 
-run_shell({CmdName, Command}, Timeout) ->
-    Port = erlang:open_port({spawn, Command}, [exit_status]),
-    shell_loop(Port, [], Timeout, {CmdName, Command}).
+do_internal({_CmdName, CmdDef}, _State) when
+    is_tuple(CmdDef) andalso
+        (erlang:tuple_size(CmdDef) < 2 orelse erlang:tuple_size(CmdDef) > 3)
+->
+    {error, "Invalid configuration: expected [{CmdName, Cmd, Opts}]."};
+do_internal({_CmdName, CmdDef}, State) when is_tuple(CmdDef) ->
+    {CmdName, Cmd, Opts} = normalize(CmdDef),
+    handle_cmd(exec(CmdName, Cmd, Opts), Opts, State);
+do_internal({undefined = _CmdName, false = _CommandFound}, _State) ->
+    {error, "No <command> (as in 'cmd <command>') given as input."};
+do_internal({CmdName, false = _CommandFound}, _State) ->
+    {error, "Command " ++ CmdName ++ " (as atom) is unknown. Check your configuration."}.
 
-shell_loop(Port, Data, Timeout, {CmdName, Command}) ->
+find_command_in(undefined = _CmdName, _Config) ->
+    false;
+find_command_in(CmdName, Config) ->
+    lists:keyfind(list_to_atom(CmdName), 1, Config).
+
+normalize({CmdName, Cmd}) ->
+    normalize({CmdName, Cmd, []});
+normalize({CmdName, Cmd, Opts}) ->
+    {atom_to_list(CmdName), Cmd, Opts}.
+
+exec(CmdName, Cmd, Opts) ->
+    Pid = self(),
+    Ref = erlang:make_ref(),
+    Timeout = get_opt(timeout, Opts, CmdName),
+    {MonitorPid, MonitorRef} = erlang:spawn_monitor(
+        fun() ->
+            not is_list(Cmd) andalso exit("call not expressed as a string"),
+            rebar_api:debug("Command ~s executing ~s with options ~p.", [CmdName, Cmd, Opts]),
+            Pid ! {Ref, os:cmd(Cmd)}
+        end
+    ),
     receive
-        {Port, {data, MoreData}} ->
-            shell_loop(Port, MoreData ++ Data, Timeout, {CmdName, Command});
-        {Port, {exit_status, 0}} ->
-            {ok, Data};
-        {Port, {exit_status, Error}} ->
-            Cmd = lists:flatten(Command),
-            {error,
-                "Command " ++
-                    CmdName ++
-                    " (" ++
-                    Cmd ++
-                    ") failed with exit status " ++
-                    integer_to_list(Error) ++ "."}
+        {Ref, Result} ->
+            {ok, {CmdName, Result}};
+        {'DOWN', MonitorRef, _Type, _Object, Info} ->
+            rebar_api:error("Command ~s died unexpectedly with ~p.", [CmdName, Info]),
+            {ok, {CmdName, died}}
     after Timeout ->
-        Cmd = lists:flatten(Command),
-        {error,
-            "Command " ++
-                CmdName ++
-                " (" ++
-                Cmd ++
-                ") timed out after " ++
-                integer_to_list(Timeout) ++ " ms."}
+        exit(MonitorPid, kill),
+        TimeOutS = integer_to_list(Timeout),
+        {error, "Command " ++ CmdName ++ " (" ++ Cmd ++ ") timed out after " ++ TimeOutS ++ " ms."}
     end.
+
+handle_cmd({error, Error}, _Opts, _State) ->
+    {error, Error};
+handle_cmd({ok, {_CmdName, died}}, _Opts, State) ->
+    {ok, State};
+handle_cmd({ok, {CmdName, Result}}, Opts, State) ->
+    case get_opt(verbose, Opts, CmdName) of
+        true ->
+            rebar_api:warn("Command ~s resulted in: ~p", [CmdName, Result]);
+        false ->
+            rebar_api:debug("Command ~s resulted in: ~p", [CmdName, Result])
+    end,
+    {ok, State}.
+
+get_opt(timeout, Opts, CmdName) ->
+    Timeout = proplists:get_value(timeout, Opts, ?DEFAULT_OPT_TIMEOUT),
+    case is_integer(Timeout) of
+        false ->
+            rebar_api:error(
+                "Command ~s's timeout not expressed as an integer. "
+                "Defaulting to ~p.",
+                [CmdName, ?DEFAULT_OPT_TIMEOUT]
+            ),
+            ?DEFAULT_OPT_TIMEOUT;
+        true ->
+            Timeout
+    end;
+get_opt(verbose, Opts, _CmdName) ->
+    proplists:get_value(verbose, Opts, ?DEFAULT_OPT_VERBOSE).
